ae4480398e2b40254b2ece24f9043901dba033cb
jdw <jdw@80x25.se>
2009-04-25 11:52:19 +0200

    Major update.

--- src/SceneBalls.cpp
+++ src/SceneBalls.cpp
@@ -5,8 +5,10 @@
  *      Author: jdw
  */
 
-#include "PixelToaster.h"
+#include "jdw_types.h"
+#include "jdw_pixel.h"
 #include "SceneBalls.h"
+
 #include <math.h>
 #include <iostream>
 
@@ -21,38 +23,50 @@ SceneBalls::SceneBalls(const iV2& in_size)
 }
 
 SceneBalls::~SceneBalls() {
-	delete[] ball;
+	for (int i = 0; i < AMOUNTOFBALLS; i++)
+		delete ball[i];
+
 	delete pNoise;
 }
 
 void SceneBalls::Update() {
+	spScreen->Fill(JDW_Pixel());
+
 	double tmp_val;
-	PixelToaster::TrueColorPixel tmp_clr;
-	iV2 tmp_halfScreen = iV2(pScreen->GetSize().x / 2, pScreen->GetSize().y / 2);
+	Pixel tmp_clr;
+	iV2 tmp_halfScreen = iV2(spScreen->GetSize().x / 2, spScreen->GetSize().y / 2);
 	iV2 tmp_pos;
 
 	ball[0]->ang -= 0.1;
-	ball[0]->SetPos(dV2(cos(ball[0]->ang) * pScreen->GetSize().x / 3 + tmp_halfScreen.x,
-	                   sin(ball[0]->ang) * pScreen->GetSize().y / 3 + tmp_halfScreen.y));
+	ball[0]->SetPos(dV2(cos(ball[0]->ang) * spScreen->GetSize().x / 3 + tmp_halfScreen.x,
+	                   sin(ball[0]->ang) * spScreen->GetSize().y / 3 + tmp_halfScreen.y));
 	ball[1]->ang += 0.1;
-	ball[1]->SetPos(dV2(cos(ball[1]->ang) * pScreen->GetSize().x / 2 + tmp_halfScreen.x,
-	                   sin(ball[1]->ang) * pScreen->GetSize().y / 2 + tmp_halfScreen.y));
+	ball[1]->SetPos(dV2(cos(ball[1]->ang) * spScreen->GetSize().x / 2 + tmp_halfScreen.x,
+	                   sin(ball[1]->ang) * spScreen->GetSize().y / 2 + tmp_halfScreen.y));
 	ball[2]->ang += 0.1;
-	ball[2]->SetPos(dV2(cos(ball[2]->ang) * pScreen->GetSize().x / 7 + tmp_halfScreen.x,
-                   sin(ball[2]->ang) * pScreen->GetSize().y / 7 + tmp_halfScreen.y));
+	ball[2]->SetPos(dV2(cos(ball[2]->ang) * spScreen->GetSize().x / 7 + tmp_halfScreen.x,
+                   sin(ball[2]->ang) * spScreen->GetSize().y / 7 + tmp_halfScreen.y));
 
 
-	for (tmp_pos.y = 0; tmp_pos.y < pScreen->GetSize().y; ++tmp_pos.y) {
-		for (tmp_pos.x = 0; tmp_pos.x < pScreen->GetSize().x; ++tmp_pos.x) {
+	for (tmp_pos.y = 0; tmp_pos.y < spScreen->GetSize().y; ++tmp_pos.y) {
+		for (tmp_pos.x = 0; tmp_pos.x < spScreen->GetSize().x; ++tmp_pos.x) {
 			tmp_val = 0.0;
 
 			for (int i = 0; i < AMOUNTOFBALLS; i++) {
 				tmp_val += ball[i]->Value(dV2(tmp_pos.x, tmp_pos.y));
 			}
 
-			tmp_clr.r = (tmp_val * 2000 > 210)? 0: tmp_val * 2000;
-			tmp_clr.r = (tmp_clr.r < 200)? 0: tmp_clr.r;
-			tmp_clr.g = tmp_clr.b = tmp_clr.r;
+			tmp_val *= 2000;
+
+			if (tmp_val > 200) {
+				tmp_clr.r = 200;
+			}
+			else if (tmp_val < 170) {
+				tmp_clr.r = tmp_clr.g = tmp_clr.b = 255;
+			}
+			else {
+				tmp_clr.r = tmp_clr.g = tmp_clr.b = 150;
+			}
 
 
 /*
@@ -60,7 +74,7 @@ void SceneBalls::Update() {
 			                          double(tmp_pos.y) / double(pScreen->GetSize().y),
 			                          0.0);
 */
-			pScreen->PutPixel(tmp_pos, tmp_clr);
+			spScreen->PutPixel(tmp_pos, tmp_clr);
 		}
 	}
 }
