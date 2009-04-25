dd2b7cc0b82c556efbb1123b46621b4400af5f15
jdw <jdw@80x25.se>
2009-04-25 11:50:06 +0200

    Adding... alot...

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,9 @@
  */
 
 #include <iostream>
+#include <vector>
+
+#include "jdw_types.h"
 
 #include "jdw_misc.h"
 #include "jdw_test.h"
@@ -14,9 +17,9 @@
 #include "jdw_vector2d.h"
 #include "jdw_improvedperlinnoise.h"
 #include "jdw_image.h"
-//#include "jdw_screen.h"
 #include "SceneBalls.h"
 #include "SceneJunk.h"
+#include "SceneTunnel.h"
 #include "jdw_list.h"
 
 using namespace PixelToaster;
@@ -25,6 +28,7 @@ namespace SceneNowPlaying {
 	enum Enum {
 		JUNK = 0,
 		BALL,
+		TUNNEL,
 
 		MAX
 	};
@@ -33,40 +37,73 @@ namespace SceneNowPlaying {
 int main() {
     iV2 size = iV2(640, 480);
 	Timer timer;
-	double t0 = timer.time();
+	double dT = timer.time(); // Start
     Display display( "t00", size.x, size.y, Output::Windowed);
-    SceneNowPlaying::Enum current = SceneNowPlaying::BALL;
+    SceneNowPlaying::Enum current = SceneNowPlaying::TUNNEL;
     JDW_Scene* pScene;
-    pScene = new SceneBalls(size);
+    pScene = new SceneTunnel(size);
+
+    PixelToaster::TrueColorPixel pixels[size.x * size.y];
+    std::vector<JDW_Pixel> fromScene;
 
     while (display.open()) {
-    	if (timer.time() - t0 < FPS) timer.wait(FPS - (timer.time() - t0));
-    	t0 = timer.time();
+    	dT = timer.delta();
+    	if (dT < FPS) timer.wait(FPS - dT);
 
     	switch (current) {
+			case SceneNowPlaying::TUNNEL: {
+				((SceneTunnel*)pScene)->SetDeltaTime(dT);
+				((SceneTunnel*)pScene)->UpdateTotalTime();
+				((SceneTunnel*)pScene)->Update();
+
+				fromScene = (*((SceneTunnel*)pScene)->GetScreen()->GetPixels());
+				for (int i = 0; i < size.x * size.y; ++i)
+					pixels[i].integer = fromScene[i].integer;
+
+				display.update(pixels);
+
+				if (pScene->GetTotalTime() > 2.0) {
+					delete (SceneTunnel*)pScene;
+					pScene = new SceneJunk(size);
+					current = SceneNowPlaying::JUNK;
+				}
+    		} break;
+
 			case SceneNowPlaying::JUNK: {
-				((SceneJunk*)pScene)->SetDeltaTime(t0);
+				((SceneJunk*)pScene)->SetDeltaTime(dT);
 				((SceneJunk*)pScene)->UpdateTotalTime();
 				((SceneJunk*)pScene)->Update();
-				display.update(((SceneJunk*)pScene)->GetImage()->GetPixels());
 
-				if (pScene->GetTotalTime() > 600.0) {
-					delete pScene;
+
+				fromScene = (*((SceneJunk*)pScene)->GetScreen()->GetPixels());
+				for (int i = 0; i < size.x * size.y; ++i)
+					pixels[i].integer = fromScene[i].integer;
+
+
+				//&pixels = pFromScene = (std::vector<PixelToaster::TrueColorPixel>*)((SceneJunk*)pScene)->GetScreen()->GetPixels();
+				display.update(pixels);
+
+				if (pScene->GetTotalTime() > 20.0) {
+					delete (SceneJunk*)pScene;
 					pScene = new SceneBalls(size);
 					current = SceneNowPlaying::BALL;
 				}
 			} break;
 
 			case SceneNowPlaying::BALL: {
-				((SceneBalls*)pScene)->SetDeltaTime(t0);
+				((SceneBalls*)pScene)->SetDeltaTime(dT);
 				((SceneBalls*)pScene)->UpdateTotalTime();
 				((SceneBalls*)pScene)->Update();
-				display.update(((SceneBalls*)pScene)->GetImage()->GetPixels());
 
-				if (pScene->GetTotalTime() > 600.0) {
-					delete pScene;
-					pScene = new SceneJunk(size);
-					current = SceneNowPlaying::JUNK;
+				fromScene = (*((SceneBalls*)pScene)->GetScreen()->GetPixels());
+				for (int i = 0; i < size.x * size.y; ++i)
+					pixels[i].integer = fromScene[i].integer;
+				display.update(pixels);
+
+				if (pScene->GetTotalTime() > 20.0) {
+					delete (SceneBalls*)pScene;
+					pScene = new SceneTunnel(size);
+					current = SceneNowPlaying::TUNNEL;
 				}
 			} break;
 
